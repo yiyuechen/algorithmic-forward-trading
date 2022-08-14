be21775b23f53a4d45e62ece4c8ed6670ab4303b
Yiyue <yiyuedove@gmail.com>
2022-08-14 11:01:01 +0800

    added comments on why we use 'continue' after we open a opsition. actually I think it's not necessary as when an order is opened, we don't need to be in a rush. have a 0.5s nap is great.

--- mt5_trading/mt5_origin.py
+++ mt5_trading/mt5_origin.py
@@ -450,7 +450,7 @@ def double_tick_strategy():
                 # sl = current_price * 1000 - rates[1][3] * 1000  # USDJPY
                 sl = current_price * 100 - rates[1][3] * 100  # BTC
                 open_request(sl_price=lower_price, type="buy", sl=sl, symbol=symbol, type_filling=type_filling)
-                continue
+                continue # if we opened an order, we go back to the beginning of the loop, we don't sleep
             if current_price < lower_price and above_or_below_sma == "below_sma" and check_retrace_when_short: # if current_price < lower_price and we are below the 25sma
                 print("sell")
                 # second_tick_high-current_price
