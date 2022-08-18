af013358a0783de7b9b3191d718f8cbc4eb06746
Yiyue <yiyuedove@gmail.com>
2022-08-19 00:14:14 +0800

    1. pass @param commission to calculate_lot_size(), open_request();   can assign commission in one place, in the main strategy() func 2. print commission and commission per lot in the output 3. added check_each_tick_close_price_above_or_below_sma().   this is the revised version of func if_above_or_below_sma() 4. added another condition for checking pause in long and short:   if higher_of_tick_2_and_3_high <= tick_1_high   if lower_of_tick_1_and_2_low >= tick_0_low 5. added sl_limit, this is used during sma_below_to_above across (or vice versa) 6. added a timer to calculate how long the program runs.   will print if an order is opened (in seconds) 7. added order opening conditions for   across_sma_from_below_to_above and across_sma_from_above_to_below,   no pause or retracement was checked in these conditions.   if sl_limit is too large, these two types of orders will be aborted

--- mt5_trading/mt5_origin.py
+++ mt5_trading/mt5_origin.py
@@ -49,19 +49,66 @@ then we don't open orders.
 14. need to check retrace func, not quite accurate
 
 15. before implementing 11, 12 ,13, 14, create branches or new files. 
-1) set buy and sell reverse. This could be profitable, especially in a consolidation, I guess. 
+1) set buy and sell reverse. This could be profitable, especially in a consolidation, I guess. (note 8/17/2022 this didn't work after a three day test. Mon-Wed)
 2) stay put buy and sell. Cancel the sma
 
 
 16. avoid consolidation. if sl and tp is too small, consider it's not a good chance? I don't know if this is a good way.
 e.g. sl < 50 points, then we don't trade
 
+17. if sma doesn't change much, say, the price of five SMAs is not far from 20 points. 
+This indicats we don't have a trend. So if we use sma and only buy when above, sell when below. it won't work
 
+
+18. if lot size too large, leverage not enough, shows "comment=No Money" error
+need to calc the maximum lot size to avoid that.
 
 question:
 waht's the Change in MT5, say 0.3%
 
+19. if almost pause, which is to say, the price is just a bit pass the previous higher price by 1-5 points (point, not pip) We may consider it 
+a valid condition.
+
+20. retracement needs to check another situation. 
+0 1 2 3 4
+previously, we check 
+1) 2 3's lower < 1 2's lower
+2) 3 4's lower < 2 3's lower
+
+but we have another situation:
+3) 1 2's low < 0, 3 does't pass two previous ticks, but 4 passes
+
+Actually, there's a better way to do the above. We get 6 ticks, just in case.
+0 1 2 3 4 5
+then we literate, pick from the third one, which is 2, call it current
+if current's low < 0 and < 1:
+    retracement = True
+
+So if previous recent ticks have a retracement, then it's a retracement.
+
+if it's tick 2 that retraced, it's a bit far a way, we need to verify if the current price has gone too high and far away from that tick 
+
+
+
+sma above or below bug
+when cross from above to below, it's still above.
+It's more often above, because above if statement is checked first and returned first
+Only when there's only 1 tick's close price > sma does the condition fail and the 'checking below sma' code have the chance to run
+
+we have fixed this bug buy revising and creating a new func
+
+Currently, the cross sma works fine. if we sell when cross up to down, and got SL'ed, which means the price goes above sma again, then we immediately buy when SL'ed. 
+
+# to do:
+a. need to check high low high low shrinking, in order to avoid consolidaiton
+
+b. utilize sma-price position checker. 
+if mixed & current price > sma, should long.
+if mixed & current price < sma, should short
+best to also check the consecutive ascending/descending ladders
+
 """
+
 
 import time
 import MetaTrader5 as mt5
@@ -170,7 +217,7 @@ def check_open_positions():
     return positions_total
 
 
-def calculate_lot_size(sl, symbol, risk_ratio=0.05, commision_per_lot=0): #sl is in points, need to convert
+def calculate_lot_size(sl, symbol, risk_ratio=0.05, commision_per_lot=4): #sl is in points, need to convert     # currently @param commision_per_lot is not included in the passed parameters
     # pip_value = 1/10**(digits-1)*contract_size?
     # EURUSD 1/(10**(5-1)) * 100000 => 10 USD
     # USDJPY 1/(10**(3-1)) * 100000 => 1000 JPY
@@ -204,8 +251,11 @@ def calculate_lot_size(sl, symbol, risk_ratio=0.05, commision_per_lot=0): #sl is
     # lot_size = (risk_ratio * capital) / (stop_loss * pip_value + commision_per_lot + spread)
     # lot_size = capital_in_risk / (stop_loss * pip_value + commision_per_lot)
     lot_size = capital_in_risk / (stop_loss * pip_value + commision_per_lot + spread/10*pip_value)
+
+
     print(f"lot size = {lot_size} before rounding")
     lot_size = round(lot_size, 2)
+    print(f"lot size = {lot_size}, commision = {lot_size*commision_per_lot}, commision_per_lot = {commision_per_lot}")
 
     if lot_size < 0.01:
         print(f"lot size = {lot_size}. Change to 0.01")
@@ -220,10 +270,10 @@ def calculate_lot_size(sl, symbol, risk_ratio=0.05, commision_per_lot=0): #sl is
     return lot_size
 
 
-def open_request(sl_price, type="buy", sl="100", symbol="USDJPY", type_filling=mt5.ORDER_FILLING_FOK):
+def open_request(sl_price, type="buy", sl="100", symbol="USDJPY", type_filling=mt5.ORDER_FILLING_FOK, commision_per_lot=4):
     
     # lot = 0.1
-    lot = calculate_lot_size(sl, symbol)
+    lot = calculate_lot_size(sl=sl, symbol=symbol, commision_per_lot=commision_per_lot) # sl, symbol, risk_ratio=0.05, commision_per_lot=4
 
     point = mt5.symbol_info(symbol).point    #EURUSD point: 1e-05   #BTCUSD point: 0.01 
     #####################
@@ -412,7 +462,7 @@ def if_above_or_below_sma(sma_list, timeframe=mt5.TIMEFRAME_M5, symbol="BTCUSD",
 
     # if above_sma == sma_list_length:
     if above_sma >= sma_list_length - 3: # 5-3 -> 2 # this should be good. If the close price goes above it or just on it, when the next tick emerges, immediately we will open an order. # This is the special rule 3
-        above_sma = "above_sma"
+        above_sma = "above"
         return above_sma
 
     i = 0
@@ -423,10 +473,87 @@ def if_above_or_below_sma(sma_list, timeframe=mt5.TIMEFRAME_M5, symbol="BTCUSD",
 
     # if below_sma == sma_list_length:
     if below_sma >= sma_list_length - 3:
-        below_sma = "below_sma"
+        below_sma = "below"
         return below_sma
 
+    # if above_sma == "above_sma" and below_sma == "below_sma":
+    #     # this means we have two close prices > the corresponding sma
+    #     # and two close prices < the corresponding sma
+    #     # so we are kinda in the middle
+    #     # the last tick's close price is the current tick's price. It's still changing, not the real close price
+    #     mixed = "mixed"
+    #     return mixed
+
     return "mixed"
+
+"""
+This is the revised version of func if_above_or_below_sma()
+return a string with a value of one of the below four:
+above, below, across_sma_from_below_to_above, across_sma_from_above_to_below
+
+also we have a list that contains five (or designated length) tick's position relative to the sma
+this is not returned
+"""
+def check_each_tick_close_price_above_or_below_sma(sma_list, timeframe=mt5.TIMEFRAME_M5, symbol="BTCUSD", start_position=0):
+    sma_list_length = len(sma_list)
+    # only get several rates, e.g., 5, not 24
+    rates = get_last_n_ticks(symbol=symbol, timeframe=timeframe, start_position=start_position, tick_count=sma_list_length)
+
+    # position = 0
+    
+    rate_close_list = []
+    for rate in rates:
+        rate_close_list.append(rate['close'])
+        
+    print(f"rate close list: {rate_close_list}")
+
+    sma_position_list = []
+
+    i = 0
+    while i < sma_list_length:
+        if rate_close_list[i] < sma_list[i]:
+            sma_position_list.append("below")
+        elif rate_close_list[i] == sma_list[i]:
+            sma_position_list.append("equal")
+        elif rate_close_list[i] > sma_list[i]:
+            sma_position_list.append("above")
+        i += 1
+
+    print(f"The position of each tick's close price: ")
+    print(sma_position_list)
+
+    # if 5 sma, then sma_list_length is 5. We have 0, 1, 2, 3, 4, these four items. 4 is the current tick. 3 is the one previous the current one
+    # sma_list_length - 1 is the current tick, sma_list_length - 2 is the one previous the current one
+    # the close price of this tick is formed. the current tick is not yet formed but still changing. 
+    # we cannot take the current tick's changing price as what we use to check if we are above or below
+
+    # if sma_position_list.count("above") >= sma_list_length-2:
+    #     position = "above"
+    # elif sma_position_list.count("below") >= sma_list_length-2:
+    #     position = "below"
+    # elif sma_position_list[sma_list_length-2] in {"above", "equal"} and sma_position_list[sma_list_length-3] == "below" and sma_position_list[sma_list_length-4] == "below":
+    #     position = "down2up_across_sma"
+    # elif sma_position_list[sma_list_length-2] in {"below", "equal"} and sma_position_list[sma_list_length-3] == "above" and sma_position_list[sma_list_length-4] == "above":
+    #     position = "up2down_across_sma"
+    
+    # ok. let's only do good and clear trades
+    # if it's ambiguous and the ticks are up and down across the sma when we cannot tell if it's above or below, we wait 
+
+    if sma_position_list.count("above") == sma_list_length: # absolute above
+        position = "above"
+    elif sma_position_list.count("below") == sma_list_length: # absolute below
+        position = "below"
+    elif sma_position_list[sma_list_length-1] == "above" and sma_position_list[sma_list_length-2] in {"above", "equal"} \
+        and sma_position_list[sma_list_length-3] == "below" and sma_position_list[sma_list_length-4] == "below" and sma_position_list[sma_list_length-5] == "below":
+        position = "across_sma_from_below_to_above"
+    elif sma_position_list[sma_list_length-1] == "below" and sma_position_list[sma_list_length-2] in {"below", "equal"} \
+        and sma_position_list[sma_list_length-3] == "above" and sma_position_list[sma_list_length-4] == "above" and sma_position_list[sma_list_length-5] == "above":
+        position = "across_sma_from_above_to_below"
+    else:
+        position = "mixed"
+
+    return position
+    
 
 def check_retrace_when_long(symbol="BTCUSD", timeframe=mt5.TIMEFRAME_M5, start_position=0, tick_count=5): 
     # 0 1 2 3 4 compare low of 2 and 3 with low of 0 and 1
@@ -456,6 +583,14 @@ def check_retrace_when_long(symbol="BTCUSD", timeframe=mt5.TIMEFRAME_M5, start_p
     if lower_price_tick_3_and_4 < lower_price_tick_1_and_2:
         retracement = True
 
+
+    # ERROR!!! need to pass *TWO* ticks
+    # but there's a third situation
+    # the 1 2'low < 0's low, but 3 fails to pass 1,2, but 4 passes
+    # if lower_price_tick_1_and_2 < tick_0_low:
+    #     retracement = True
+    #     print("lower_price_tick_1_and_2 < tick_0_low")
+
     print(f"retracement: {retracement}")
     return retracement
 
@@ -480,6 +615,13 @@ def check_retrace_when_short(symbol="BTCUSD", timeframe=mt5.TIMEFRAME_M5, start_
     if higher_price_tick_3_and_4 > higher_price_tick_1_and_2:
         retracement = True
 
+    # # ERROR!!! need to pass *TWO* ticks
+    # # but there's a third situation
+    # # the 1 2'high > 0's high, but 3 fails to pass 1,2, but 4 passes
+    # if higher_price_tick_1_and_2 > tick_0_high:
+    #     retracement = True
+    #     print("higher_price_tick_1_and_2 > tick_0_high")
+
     print(f"retracement: {retracement}")
     return retracement
 
@@ -493,7 +635,14 @@ def check_pause_when_long(symbol="BTCUSD", timeframe="TIMEFRAME_M5", start_posit
     higher_of_tick_0_and_1_high = compare_two_and_get_higher(tick_0_high, tick_1_high)
     higher_of_tick_2_and_3_high = compare_two_and_get_higher(tick_2_high, tick_3_high)
     # if higher_of_tick_2_and_3_high <= higher_of_tick_0_and_1_high:
-    if higher_of_tick_2_and_3_high <= tick_1_high:
+    if higher_of_tick_2_and_3_high <= tick_1_high: # tick 1 high + 5 points give it some space
+        pause = True
+
+    # another situation, need to realize
+    # if 0 highest, 1 < 2 < 0, 3 < 2, 4 > 3&2:
+    # namely, if 1,2's higher < 0's high
+    higher_of_tick_1_and_2_high = compare_two_and_get_higher(tick_1_high, tick_2_high)
+    if higher_of_tick_1_and_2_high <= tick_0_high:
         pause = True
 
     print(f"higher_of_tick_0_and_1_high: {higher_of_tick_0_and_1_high}")
@@ -515,6 +664,13 @@ def check_pause_when_short(symbol="BTCUSD", timeframe=mt5.TIMEFRAME_M5, start_po
     if lower_of_tick_2_and_3_low >= tick_1_low: # seems tick_1_low will do the job
         pause = True
     
+    # another situation, need to realize
+    # if 0 lowest, 1 > 2 > 0, 3 > 2, 4 < 3&2:
+    # namely, if 1,2's low < 0's low
+    lower_of_tick_1_and_2_low = compare_two_and_get_lower(tick_1_low, tick_2_low)
+    if lower_of_tick_1_and_2_low >= tick_0_low:
+        pause =True
+
     print(f"lower_of_tick_0_and_1_low: {lower_of_tick_0_and_1_low}")
     print(f"lower_of_tick_2_and_3_low: {lower_of_tick_2_and_3_low}")
     print(f"pause: {pause}")
@@ -577,9 +733,18 @@ def double_tick_strategy():
     # type_filling = mt5.ORDER_FILLING_IOC # IC
     type_filling = mt5.ORDER_FILLING_FOK # FXTM
     timeframe = mt5.TIMEFRAME_M5
+    sl_limit = 300 # points for USDJPY
+
+    # specify the commision for each lot here and make sure to pass it in the following open_request() functions
+    # currently it is not included in the parameters
+    commision_per_lot = 4
+
+    # used to print how many seconds it runs, 
+    # also if the program freezes, the print output will not change, which draws us attention
+    timer = 0
 
     while True:
-        
+
         open_positions = check_open_positions()
         if open_positions == 0:
             # rates <class 'numpy.ndarray'>
@@ -597,15 +762,22 @@ def double_tick_strategy():
             tick_two_low = rates[1][3]
             lower_price = compare_two_and_get_lower(tick_one_low, tick_two_low)
 
+            tick_two_close = rates[1]['close']
+
             #sma = calculate_current_sma(symbol="BTCUSD", sma_length=24)
             
             sma_list = calculate_sma_of_latest_n_ticks(symbol=symbol, timeframe=timeframe, sma_length=24, sma_count=5)
             
-            above_or_below_sma = if_above_or_below_sma(sma_list, timeframe= timeframe, symbol=symbol, start_position=0)
+            # # v1 
+            # above_or_below_sma = if_above_or_below_sma(sma_list, timeframe=timeframe, symbol=symbol, start_position=0)
+            # print(f"above or below sma: {above_or_below_sma}")
+
+            # v2
+            above_or_below_sma = check_each_tick_close_price_above_or_below_sma(sma_list, timeframe=timeframe, symbol=symbol, start_position=0)
             print(f"above or below sma: {above_or_below_sma}")
             
             # if current_price > higher_price, and we are above the 24sma, and there's a retracement
-            if current_price > higher_price and above_or_below_sma == "above_sma" and check_retrace_or_pause_when_long(symbol=symbol, timeframe=timeframe, start_position=0, tick_count=5):
+            if current_price > higher_price and above_or_below_sma == "above" and check_retrace_or_pause_when_long(symbol=symbol, timeframe=timeframe, start_position=0, tick_count=5):
                 print("buy")
                 # sl = current_price * 1000 - rates[1][3] * 1000  # USDJPY
                 # BTC digits -> 2   USDJPY digits -> 3
@@ -615,9 +787,9 @@ def double_tick_strategy():
                 sl = current_price * multiply_digits - lower_price * multiply_digits  # BTC
                 # sl = current_price * 100 - lower_price * 100  # BTC
                 
-                open_request(sl_price=lower_price, type="buy", sl=sl, symbol=symbol, type_filling=type_filling)
+                open_request(sl_price=lower_price, type="buy", sl=sl, symbol=symbol, type_filling=type_filling, commision_per_lot=commision_per_lot)
                 # continue # if we opened an order, we go back to the beginning of the loop, we don't sleep
-            elif current_price < lower_price and above_or_below_sma == "below_sma" and check_retrace_or_pause_when_short(symbol=symbol, timeframe=timeframe, start_position=0, tick_count=5): # if current_price < lower_price and we are below the 25sma
+            elif current_price < lower_price and above_or_below_sma == "below" and check_retrace_or_pause_when_short(symbol=symbol, timeframe=timeframe, start_position=0, tick_count=5): # if current_price < lower_price and we are below the 25sma
                 print("sell")
                 # second_tick_high-current_price
                 # sl = rates[1][2] * 1000 - current_price * 1000  # USDJPY
@@ -625,10 +797,62 @@ def double_tick_strategy():
                 multiply_digits = 10 ** digits
                 sl = higher_price * multiply_digits - current_price * multiply_digits  # BTC
                 # sl = higher_price * 100 - current_price * 100  # BTC
-                open_request(sl_price=higher_price, type="sell", sl=sl, symbol=symbol, type_filling=type_filling)
+                open_request(sl_price=higher_price, type="sell", sl=sl, symbol=symbol, type_filling=type_filling, commision_per_lot=commision_per_lot)
                 # continue
+
+            # do we need to check retrace or pause when doing across sma trades?
+            # maybe not. because usually it's the price goes down from above to below and then go above, so there should be a natural retrace/reverse
+            # actually it's best to include, maybe. bacause if the reverse point is too far away (in this case maybe no retrace nearby the sma), 
+            # the price would touch the sma and then bounce back, continue its previous trend
+
+            # across_sma_from_below_to_above, 
+            elif current_price > tick_two_close and above_or_below_sma == "across_sma_from_below_to_above": # and check_retrace_or_pause_when_long(symbol=symbol, timeframe=timeframe, start_position=0, tick_count=5):
+                print("buy")
+                # sl = current_price * 1000 - rates[1][3] * 1000  # USDJPY
+                # BTC digits -> 2   USDJPY digits -> 3
+                digits = mt5.symbol_info(symbol).digits # BTC digits -> 2         mt5.symbol_info(symbol).xxx, not mt5.symbol_info_tick(symbol).xxx
+                multiply_digits = 10 ** digits
+                # sl is in points, /10 if needed to convert to pips
+                sl = current_price * multiply_digits - lower_price * multiply_digits  # BTC
+                # sl = current_price * 100 - lower_price * 100  # BTC
+
+                # hard-coded for USD/JPY
+                if sl >= sl_limit: # if sl > 300 points, or 30 pips
+                    print(f"sl is {sl} points. too large. aborted.")
+                    continue
+                
+                open_request(sl_price=lower_price, type="buy", sl=sl, symbol=symbol, type_filling=type_filling, commision_per_lot=commision_per_lot)
+                # continue # if we opened an order, we go back to the beginning of the loop, we don't sleep
+            # across_sma_from_above_to_below
+            elif current_price < tick_two_close and above_or_below_sma == "across_sma_from_above_to_below": # and check_retrace_or_pause_when_short(symbol=symbol, timeframe=timeframe, start_position=0, tick_count=5):
+                print("sell")
+                # second_tick_high-current_price
+                # sl = rates[1][2] * 1000 - current_price * 1000  # USDJPY
+                digits = mt5.symbol_info(symbol).digits
+                multiply_digits = 10 ** digits
+                sl = higher_price * multiply_digits - current_price * multiply_digits  # BTC
+                # hard-coded for USD/JPY
+                if sl >= sl_limit: # if sl > 300 points, or 30 pips
+                    print(f"sl is {sl} points. too large. aborted.")
+                    continue
+
+                # sl = higher_price * 100 - current_price * 100  # BTC
+                open_request(sl_price=higher_price, type="sell", sl=sl, symbol=symbol, type_filling=type_filling, commision_per_lot=commision_per_lot)
+                # continue
+
         # time.sleep(0.1)
+        else:
+            # open_positions > 0
+            # sys.stdout.write(".")
+            # sys.stdout.flush()
+            print(f"\t{timer} s", end="\r", flush=True)
+            
+
         time.sleep(0.5)
+        # os.system('cls') # this will clean all the output, not what we expect
+        timer += 0.5
+
+
 
 
     # rates = get_last_three_ticks()
