b3a94364e4e0f4095202381429b0be7424b1f3dc
Yiyue <yiyuedove@gmail.com>
2026-08-27 22:23:38 +0100

    Add credential_info.py example

---
+++ mt5_trading/credential_info.example.py
@@ -0,0 +1,11 @@
+live_account_id = 123456789
+password = 'YOUR_PASSWORD'
+server_live = 'YOUR_BROKER_SERVER'
+
+demo_account_id = 123456789
+password2 = 'YOUR_PASSWORD'
+server_demo = 'YOUR_BROKER_SERVER'
+
+saved_last_manually_input_account_id = 123456789
+saved_last_manually_input_account_password = 'YOUR_PASSWORD'
+saved_last_manually_input_account_server = 'YOUR_BROKER_SERVER'
